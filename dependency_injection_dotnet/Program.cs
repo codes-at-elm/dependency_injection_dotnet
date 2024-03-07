482a44d5d479c890e7574714b8c1cbb1e97e6dd4
Randhir Kumar Yadav <yrandhir@outlook.com>
2024-03-07 16:27:08 -0600

    Created new console application named dependency_injection_dotnet.

---
+++ dependency_injection_dotnet/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dependency_injection_dotnet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
